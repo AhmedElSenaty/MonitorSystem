2a4224f50ed750ac78151782ac853e9012cb90e9
Habiba <habiba.elshaer004@gmail.com>
2025-04-16 23:56:51 +0200

    footer first draft

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Footer from "./components/footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <Footer/>
         </>
     );
 };
