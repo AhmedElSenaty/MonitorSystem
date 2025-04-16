2971f4dfc19945b530fdc2711f965ba9cb317479
Habiba <habiba.elshaer004@gmail.com>
2025-04-16 23:49:17 +0200

    navbar first draft

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <Navbar />
         </>
     );
 };
