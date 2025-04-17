e974b29667f870c625eacaba864fcee62cd7479e
Ahmed El Senaty <ahmedmohamedomarelsenaty@gmail.com>
2025-04-17 20:15:12 +0200

    Done: form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,29 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bs-stepper/dist/css/bs-stepper.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import "./App.css";
+
+import { BrowserRouter, Route, Routes } from 'react-router';
+import RegisterForm from './pages/register/Register';
 
 const App = () => {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<UnderDevelopment />} />
+                <Route path="/register" element={<RegisterForm />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
 export default App;
+
+const UnderDevelopment = () => {
+    return (
+        <div className='h-screen flex items-center justify-center m-5 text-center'>
+            <h1 className='alert alert-primary text-3xl font-bold'>Under Development</h1>
+        </div>
+    )
+}
